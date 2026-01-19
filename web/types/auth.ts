a43219284e64227865d152146ad9f237ec579104
WebCodeSmith <vf813250@gmail.com>
2026-01-19 12:40:30 -0300

    fixes

--- web/types/auth.ts
+++
@@ -1,9 +0,0 @@
-import { User } from './user'
-
-export interface AuthContextType {
-  user: User | null
-  login: (email: string, senha: string) => Promise<boolean>
-  logout: () => void
-  loading: boolean
-}
-
